c54c40e8476657a05e8e0b60f789d5a327a79cad
pikka1999 <hikarix1999@gmail.com>
2022-02-02 15:53:12 +0000

    リスト機能作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,8 @@
 class ApplicationController < ActionController::Base
-     before_action :authenticate_user!
+    #  before_action :authenticate_user!
      
-      protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
 
-  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
